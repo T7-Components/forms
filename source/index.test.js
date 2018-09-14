8ba28adf3397c7bd0d5e608ad954d92a6e6728b2
nathansmith <nathan@sonspring.com>
2018-09-14 15:40:15 -0500

    Updating unit test/build.

--- source/index.test.js
+++ source/index.test.js
@@ -1,7 +1,37 @@
-// TODO.
-describe('TODO', () => {
-  it('TODO', () => {
-    expect(true)
-      .toBe(true)
+// Dependencies.
+import * as all from './'
+
+// Describe `<Component/>` name.
+describe('T7 Forms -- index.js', () => {
+  // Expected components.
+  const componentList = [
+    'Button',
+    'Checkbox',
+    'Input',
+    'Label',
+    'Radio',
+    'Select',
+    'Textarea',
+    'Textdiv'
+  ]
+
+  // ==========================
+  // Test for component export.
+  // ==========================
+
+  it('exports React components', () => {
+    // Loop through.
+    Object.keys(all).forEach((key) => {
+      // Get component.
+      const component = all[key]
+
+      // Expected component?
+      expect(componentList)
+        .toContain(component.name)
+
+      // Only functions?
+      expect(typeof component)
+        .toBe('function')
+    })
   })
 })
